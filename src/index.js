4126310ba86428970924dfb155060d87202ef5a8
FrontenderCris <gansocc@gmail.com>
2017-04-03 15:38:29 +1200

    Day 6 App optimize the layout and use editor component..

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
-import UserAddPage from './pages/UserAdd';
 import HomePage from './pages/Home';
-
+import UserList from './pages/UserList';
+import UserAddPage from './pages/UserAdd';
+import UserEditPage from './pages/UserEdit';
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={HomePage}/>
         <Route path="/user/add" component={UserAddPage}/>
+        <Route path="/user/list" component={UserList}/>
+        <Route path="/user/edit/:id" component={UserEditPage}/>
     </Router>
 ), document.getElementById('app'));
