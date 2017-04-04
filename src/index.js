edd972b3f64f892df62bd35ddee0e580a74fe520
Cris <gansocc@gmail.com>
2017-04-04 13:35:44 +1200

    Day 7 1.Book add/edit 2.AutoComplete

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import HomePage from './pages/Home';
 import UserList from './pages/UserList';
 import UserAddPage from './pages/UserAdd';
 import UserEditPage from './pages/UserEdit';
+import BookListPage from './pages/BookList';
+import BookAddPage from './pages/BookAdd';
+import BookEditPage from './pages/BookEdit';
+
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={HomePage}/>
         <Route path="/user/add" component={UserAddPage}/>
         <Route path="/user/list" component={UserList}/>
         <Route path="/user/edit/:id" component={UserEditPage}/>
+        <Route path="/book/list" component={BookListPage}/>
+        <Route path="/book/add" component={BookAddPage}/>
+        <Route path="/book/edit/:id" component={BookEditPage}/>
     </Router>
 ), document.getElementById('app'));
---
+++ src/pages/BookAdd.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Cris on 2017/4/4.
+ */
+import React from 'react';
+import HomeLayout from '../layouts/HomeLayout';
+import BookEditor from '../components/BookEditor';
+
+class BookAdd extends React.Component {
+    render () {
+        return (
+            <HomeLayout title="添加图书">
+                <BookEditor/>
+            </HomeLayout>
+        );
+    }
+}
+
+export default BookAdd;
